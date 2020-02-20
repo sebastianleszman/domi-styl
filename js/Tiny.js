11f7098d37bcc6057710145a29fa089e08c6b135
Sebastian Leszman <sebastianleszman@gmail.com>
2020-02-20 08:18:57 +0100

    another commit

--- js/Tiny.js
+++ js/Tiny.js
@@ -1,8 +1,30 @@
-let slider = tns({
+let carousel = tns({
   items: 1,
   controls: false,
   arrowKeys: false,
-  container: '.tiny-slider',
+  container: '.carousel-slider',
+  swipeAngle: false,
+  speed: 1000,
+  mouseDrag: true,
+  slideBy: 1,
+  gutter: 0,
+  autoplay: true,
+  autoplayHoverPause: false,
+  preventActionWhenRunning: true,
+  autoplayTimeout: 4000,
+  autoplayButtonOutput: false, // hides autoplay buttons
+  nav: false, //hides navbar which is almost transparent
+  autoplayText: ['▶', '❚❚'],
+  autoHeight: true,
+  prevButton: false,
+  nextButton: false
+});
+
+let trust = tns({
+  items: 1,
+  controls: false,
+  arrowKeys: false,
+  container: '.trust-slider',
   swipeAngle: false,
   speed: 1000,
   mouseDrag: true,
@@ -11,7 +33,7 @@ let slider = tns({
   autoplay: true,
   autoplayHoverPause: false,
   preventActionWhenRunning: true,
-  autoplayTimeout: 4000,
+  autoplayTimeout: 1500,
   autoplayButtonOutput: false, // hides autoplay buttons
   nav: false, //hides navbar which is almost transparent
   autoplayText: ['▶', '❚❚'],
