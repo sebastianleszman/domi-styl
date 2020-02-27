5519e2860997b505d5235e8cdd7a04580944feb8
Sebastian Leszman <sebastianleszman@gmail.com>
2020-02-27 10:11:47 +0100

    another commit

--- js/App.js
+++ js/App.js
@@ -49,3 +49,16 @@ let trust = tns({
     }
   }
 });
+
+function initMap() {
+  // The location of melgiew
+  let melgiew = { lat: 51.196247, lng: 22.787431 };
+  // The map, centered at melgiew
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: melgiew
+  });
+
+  // The marker
+  let marker = new google.maps.Marker({ position: melgiew, map: map });
+}
