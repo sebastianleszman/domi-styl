a42ffdb47bda90819afff08f68048ecefabca34e
Sebastian Leszman <sebastianleszman@gmail.com>
2020-05-20 09:37:08 +0200

    another commit

--- js/App.js
+++ js/App.js
@@ -2,7 +2,7 @@ let carousel = tns({
   items: 1,
   controls: false,
   arrowKeys: false,
-  container: '.carousel-slider',
+  container: ".carousel-slider",
   swipeAngle: false,
   speed: 1000,
   slideBy: 1,
@@ -19,7 +19,7 @@ let trust = tns({
   items: 1,
   controls: false,
   arrowKeys: false,
-  container: '.trust-slider',
+  container: ".trust-slider",
   swipeAngle: false,
   speed: 1000,
   slideBy: 1,
@@ -30,7 +30,7 @@ let trust = tns({
   autoplayTimeout: 1500,
   autoplayButtonOutput: false, // hides autoplay buttons
   nav: false, //hides navbar which is almost transparent
-  autoplayText: ['▶', '❚❚'],
+  autoplayText: ["▶", "❚❚"],
   responsive: {
     620: {
       items: 2
@@ -45,10 +45,41 @@ function initMap() {
   // The location of melgiew
   let melgiew = { lat: 51.196247, lng: 22.787431 };
   // The map, centered at melgiew
-  let map = new google.maps.Map(document.getElementById('map'), {
+  let map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: melgiew
   });
   // The marker
   let marker = new google.maps.Marker({ position: melgiew, map: map });
 }
+
+//hamburger effect
+const hamburger = document.querySelector(".hamburger");
+const menuMain = document.querySelector(".menu__main");
+
+const handleClick = () => {
+  hamburger.classList.toggle("hamburger--active");
+  menuMain.classList.toggle("menu__main--active");
+};
+
+hamburger.addEventListener("click", handleClick);
+
+//dropdown effect
+const dropdown = document.getElementsByClassName("dropdown");
+
+let i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("dropdown--active");
+    let dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+const galleryTittle = document.querySelector(".gallery__title");
+
---
+++ js/gallery.js
@@ -0,0 +1,233 @@
+//define ui variable
+const biurowe = document.querySelector(".biurowe");
+const gabinetowe = document.querySelector(".gabinetowe");
+const konferencyjne = document.querySelector(".konferencyjne");
+const szafyMetaloweUbraniowe = document.querySelector(
+  ".szafyMetaloweUbraniowe"
+);
+const regalyWarsztatowe = document.querySelector(".regalyWarsztatowe");
+const szafkiMetaloweZamykane = document.querySelector(
+  ".szafkiMetaloweZamykane"
+);
+const foteleObrotowe = document.querySelector(".foteleObrotowe");
+const foteleKonferencyjne = document.querySelector(".foteleKonferencyjne");
+const sofy = document.querySelector(".sofy");
+const kanapy = document.querySelector(".kanapy");
+const narozniki = document.querySelector(".narozniki");
+const fotele = document.querySelector(".fotele");
+const krzeslaSzkolne = document.querySelector(".krzeslaSzkolne");
+const lawki = document.querySelector(".lawki");
+const biurkaNauczyciel = document.querySelector(".biurkaNauczyciel");
+const szafkiSzkolne = document.querySelector(".szafkiSzkolne");
+const szafkiRegaly = document.querySelector(".szafkiRegaly");
+const lady = document.querySelector(".lady");
+const zestawyAneksow = document.querySelector(".zestawyAneksow");
+const szafkiWiszace = document.querySelector(".szafkiWiszace");
+const szafkiStojace = document.querySelector(".szafkiStojace");
+const blaty = document.querySelector(".blaty");
+const stoly = document.querySelector(".stoly");
+
+// const imgPanel = document.querySelector('.img-panels');
+
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+const img3 = document.querySelector("#img3");
+const img4 = document.querySelector("#img4");
+
+// load all event listneres
+loadEventListeners();
+
+// load all event listneres
+function loadEventListeners() {
+  biurowe.addEventListener("click", changeImagesBiurowe);
+  gabinetowe.addEventListener("click", changeImagesGabinetowe);
+  konferencyjne.addEventListener("click", changeImageskonferencyjne);
+  szafyMetaloweUbraniowe.addEventListener(
+    "click",
+    changeImagesszafyMetaloweUbraniowe
+  );
+  regalyWarsztatowe.addEventListener("click", changeImagesregalyWarsztatowe);
+  szafkiMetaloweZamykane.addEventListener(
+    "click",
+    changeImagesszafkiMetaloweZamykane
+  );
+  foteleObrotowe.addEventListener("click", changeImagesfoteleObrotowe);
+  foteleKonferencyjne.addEventListener(
+    "click",
+    changeImagesfoteleKonferencyjne
+  );
+  sofy.addEventListener("click", changeImagessofy);
+  kanapy.addEventListener("click", changeImageskanapy);
+  narozniki.addEventListener("click", changeImagesnarozniki);
+  fotele.addEventListener("click", changeImagesfotele);
+  krzeslaSzkolne.addEventListener("click", changeImageskrzeslaSzkolne);
+  lawki.addEventListener("click", changeImageslawki);
+  biurkaNauczyciel.addEventListener("click", changeImagesbiurkaNauczyciel);
+  szafkiSzkolne.addEventListener("click", changeImagesszafkiSzkolne);
+  szafkiRegaly.addEventListener("click", changeImagesszafkiRegaly);
+  lady.addEventListener("click", changeImageslady);
+  zestawyAneksow.addEventListener("click", changeImageszestawyAneksow);
+  szafkiWiszace.addEventListener("click", changeImagesszafkiWiszace);
+  szafkiStojace.addEventListener("click", changeImagesszafkiStojace);
+  blaty.addEventListener("click", changeImagesblaty);
+  stoly.addEventListener("click", changeImagesstoly);
+}
+
+function changeImagesBiurowe() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+function changeImagesGabinetowe() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+function changeImageskonferencyjne() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesszafyMetaloweUbraniowe() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesregalyWarsztatowe() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesszafkiMetaloweZamykane() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesfoteleObrotowe() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesfoteleKonferencyjne() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagessofy() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImageskanapy() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesnarozniki() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesfotele() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImageskrzeslaSzkolne() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImageslawki() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesbiurkaNauczyciel() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesszafkiSzkolne() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesszafkiRegaly() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImageslady() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImageszestawyAneksow() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesszafkiWiszace() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesszafkiStojace() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesblaty() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
+
+function changeImagesstoly() {
+  img1.src = "../asets/design.jpg";
+  img2.src = "../asets/design.jpg";
+  img3.src = "../asets/design.jpg";
+  img4.src = "../asets/design.jpg";
+}
